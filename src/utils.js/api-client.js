a8ace08046ff2be12148f9d177631ca6e74ea9aa
Dominique <68824927+dpendjol@users.noreply.github.com>
2020-12-28 14:59:10 +0100

    Update api-client.js
    Forgot to comment out the sections that only work when you implement the api-info,js file.

--- src/utils.js/api-client.js
+++ src/utils.js/api-client.js
@@ -7,21 +7,24 @@
 
 /*********************************************************** */
 
+/*** Commented this out to prevent errors
 import API_ENDPOINT from './api-info';
+*/
 import { responseGoogle } from '../response/responseSpreadsheet';
 
-/**
+/** 
  * Get the values from the sheet. For testing purposes saved the response to a file 
  * called responseSpreadsheet.
  */
-export const getData = () => {
-    fetch(API_ENDPOINT)
-        .then(data => data.json())
-        .then(data => {
-            return data.values
-        })
-        .catch(err => console.log(err));
-}
+//Code only used when getting live data
+//export const getData = () => {
+//    fetch(API_ENDPOINT)
+//        .then(data => data.json())
+//        .then(data => {
+//            return data.values
+//        })
+//        .catch(err => console.log(err));
+//}
 
 export const getMockData = () => {
     return responseGoogle;
